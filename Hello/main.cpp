1155ae0962ba44bfdf21e05f291fe44f3ac254f8
sethchens <bryan31032@gmail.com>
2024-09-30 08:38:07 -0600

    Initial Commit

---
+++ Hello/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Hello
+//
+//  Created by Seth on 9/30/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+   return 0;
+}
